5a5f3185716c4ecd74b5e2be397027cd4c3a735c
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-16 01:25:51 +0330

    add promise

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 const posts = [{ title: "post 1" }, { title: "post 2" }];
 const ul = document.querySelector("#app ul");
+let bool = false;
 
-const createPost = (post, callback) => {
-  setTimeout(() => {
-    // code
-    posts.push(post);
+const createPost = (post) => {
+  // code
+  return new Promise((resolve, reject) => {
+    if (bool) {
+      // code
+      setTimeout(() => {
+        posts.push(post);
 
-    callback();
-  }, 2000);
+        resolve(bool);
+      }, 1000);
+    } else {
+      reject(bool);
+    }
+  });
 };
 
 const getPosts = () => {
-  setTimeout(() => {
-    // code
-    let output = "";
+  // code
+  let output = "";
 
-    posts.forEach((post) => {
-      output += `<li>${post.title}</li>`;
-    });
+  posts.forEach((post) => {
+    output += `<li>${post.title}</li>`;
+  });
 
-    ul.innerHTML = output;
-  }, 1000);
+  ul.innerHTML = output;
 };
 
-createPost({ title: "post 3" }, getPosts);
+createPost({ title: "post 3" })
+  .then((data) => {
+    console.log(data);
+    return createPost({ title: "post 4" });
+  })
+  .then((data) => {
+    console.log(data);
+    return createPost({ title: "post 5" });
+  })
+  .then((data) => {
+    console.log(data);
+    return createPost({ title: "post 6" });
+  })
+  .then((data) => {
+    console.log(data);
+    getPosts();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
