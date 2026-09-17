1aac901f63ca4481f52ded809954301e8ebc4c19
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-17 20:36:23 +0330

    add Ajax (methed get, json data)

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const btn = document.querySelector("button");
-const p = document.querySelector("p");
+const btnPost = document.querySelector("#post");
+const btnPosts = document.querySelector("#posts");
+const ul = document.querySelector("ul");
 
-btn.onclick = () => {
+btnPost.onclick = () => {
+  // code
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "data.txt");
+  xhr.open("GET", "post.json");
 
   xhr.onload = () => {
-    xhr.status === 200
-      ? (p.innerText = xhr.responseText)
-      : console.log("Error");
+    const post = JSON.parse(xhr.responseText)[0];
+    const liCreate = `<li>${post.title}</li>`;
+
+    xhr.status === 200 ? (ul.innerHTML = liCreate) : console.log("Error");
+  };
+
+  xhr.send();
+};
+
+btnPosts.onclick = () => {
+  // code
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "posts.json");
+
+  xhr.onload = () => {
+    const posts = JSON.parse(xhr.responseText);
+    let liCreates = "";
+
+    posts.forEach((post) => {
+      liCreates += `<li>${post.title}</li>`;
+    });
+
+    xhr.status === 200 ? (ul.innerHTML = liCreates) : console.log("Error");
   };
 
   xhr.send();
