2c771d9ae3acbf4b44448fbc253ef54d2ef984ed
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-17 19:48:01 +0330

    add Ajax (methed text plain)

--- app.js
+++ app.js
@@ -1,62 +1,16 @@
-const posts = [{ title: "post 1" }, { title: "post 2" }];
-const ul = document.querySelector("#app ul");
-let bool = true;
+const btn = document.querySelector("button");
+const p = document.querySelector("p");
 
-const createPost = (post) => {
-  // code
-  return new Promise((resolve, reject) => {
-    if (bool) {
-      // code
-      setTimeout(() => {
-        posts.push(post);
+btn.onclick = () => {
+  const xhr = new XMLHttpRequest();
 
-        resolve(bool);
-      }, 1000);
-    } else {
-      reject(bool);
-    }
-  });
+  xhr.open("GET", "data.txt");
+
+  xhr.onload = () => {
+    xhr.status === 200
+      ? (p.innerText = xhr.responseText)
+      : console.log("Error");
+  };
+
+  xhr.send();
 };
-
-const addPosts = [
-  createPost({ title: "post 4" }),
-  createPost({ title: "post 5" }),
-  createPost({ title: "post 6" }),
-  createPost({ title: "post 7" }),
-];
-
-Promise.all(addPosts)
-  .then((addpost) => {
-    console.log(addpost);
-
-    let output = "";
-
-    posts.forEach((post) => {
-      output += `<li>${post.title}</li>`;
-    });
-
-    ul.innerHTML = output;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// async function getPosts() {
-//   // code
-//   try {
-//     const addpost = await Promise.all(addPosts);
-//     console.log(addpost);
-
-//     let output = "";
-
-//     posts.forEach((post) => {
-//       output += `<li>${post.title}</li>`;
-//     });
-
-//     ul.innerHTML = output;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getPosts();
