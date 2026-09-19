63a32ce11eeda6b5f04b2bceda02660b07803703
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-19 20:53:27 +0330

    add fetch (methed get, api)

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const btn = document.querySelector("button");
 const ul = document.querySelector("ul");
+const loading = document.querySelector("#app div");
+loading.classList.add("d-none");
 
 // btn.onclick = () => {
-//   fetch("posts.json")
-//     .then((res) => res.json())
+//   loading.classList.remove("d-none");
+
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => {
+//       return res.json();
+//     })
 //     .then((posts) => {
 //       // codes
+//       loading.classList.add("d-none");
+
+//       let output = "";
+//       posts.forEach((post) => {
+//         output += `<li>${post.title}</li>`;
+//       });
+
+//       ul.innerHTML = output;
 //     })
 //     .catch((err) => console.log(err));
 // };
 
 btn.onclick = async function () {
   try {
-    const res = await fetch("posts.json");
+    loading.classList.remove("d-none");
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await res.json();
+
+    loading.classList.add("d-none");
 
     let output = "";
     posts.forEach((post) => {
